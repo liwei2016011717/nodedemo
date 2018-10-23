e40fc1cd76d2c52e69fba9f42dca46301a94e961
wangding <408542507@qq.com>
2018-10-23 17:18:47 +0800

    12-child-process

---
+++ 12-child-process/01-exec-file.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const cp=require('child_process');
+
+var cmd=process.argv[2],
+    arg=[];
+
+for(var i=3;i<process.argv.length;i++) arg.push(process.argv[i]);
+
+
+/*
+cp.execFile('cat',['01-exec-file.js'],(err,out,error)=>{
+  if(err){
+    console.error(error);
+    process.exit(100);
+  }
+  console.log(out);
+})
+*/
+
+cp.execFile(cmd,argv,(err,out,error)=>{
+  if(err){
+    console.error(error);
+    process.exit(100);
+  }
+  console.log(out);
+})
